206cedb2020d3ed73baa9f0ca9507295d85836b5
Zynaptic-Scripts <Zynaptic.scripts@gmail.com>
2025-06-27 01:59:53 -0400

    Update InterfaceManager.lua

--- Addons/InterfaceManager.lua
+++ Addons/InterfaceManager.lua
@@ -66,7 +66,7 @@ local InterfaceManager = {} do
 
 		local InterfaceTheme = section:AddDropdown("InterfaceTheme", {
 			Title = "Theme",
-			Description = "     ",
+			Description = "        ",
 			Values = Library.Themes,
 			Default = Settings.Theme,
 			Callback = function(Value)
